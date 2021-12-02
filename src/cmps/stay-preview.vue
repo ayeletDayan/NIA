2fbd3eb1cb693bdcd28f7bd04213ad0a39bb7af2
Naama Atias <naamazch@gmail.com>
2021-12-02 10:46:47 +0200

    naama-list

--- src/cmps/stay-preview.vue
+++ src/cmps/stay-preview.vue
@@ -1,11 +1,11 @@
 
 <template>
-    <section class="stay-preview flex">
+    <section class="stay-preview">
       <img :src="stay.imgUrls[0]" alt="" />
-      <!-- <p v-if="stay.reviews.length">⭐{{avgRate}}<span>({{stay.reviews.length}})</span></p>
+      <p v-if="stay.reviews.length">⭐{{avgRate}}<span>({{stay.reviews.length}})</span></p>
       <p v-else-if="stay.reviews.length < 3">❤ {{stay.reviews.length}} reviews</p>
-      <p v-else-if="newStay&&!stay.reviews.length">⭐ New</p>
-      <p v-else >No reviews yet</p> -->
+      <!-- <p v-else-if="newStay&&!stay.reviews.length">⭐ New</p> -->
+      <p v-else >No reviews yet</p>
       <h4>{{ stay.type }}</h4>
       <h4>{{ stay.name }}</h4>
       <h4><strong>{{ stay.price }} $</strong> / night</h4>
@@ -22,13 +22,20 @@ export default {
   },
   data() {
     return {
-      avgRate: 0
-    }
+      avgRate: 0,
+      currStay: this.stay,
+    };
   },
   created() {
-    avgRate = this.$store.getters.avgRate;
+    this.loadCurrReviews(this.currStay);
+    this.avgRate = this.$store.getters.avgRate;
     // isNewStay = this.$store.commit({})
-  }
+  },
+  methods: {
+    loadCurrReviews(stay) {
+      this.$store.commit({ type: "setStayReviews", stay });
+    },
+  },
 
 
 }
