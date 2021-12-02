d77c7d0d51ff528dcf1d23b769db05627c7b78d1
Itzik Dahan <dahanzik@gmail.com>
2021-12-02 22:12:33 +0200

    2/12/21 22:15

---
+++ src/cmps/stay-amenity.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <img :src="symbol" alt="">
+    <span>{{ item }}</span>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+  computed: {
+    symbol() {
+      var symbol = "";
+      if (this.item === "TV") return symbol = "@/assets/img/tv.png";
+      if (this.item === "Wifi") return symbol = "@/assets/img/wifi.png";
+      if (this.item === "Kitchen") return symbol = "@/assets/img/kitchen.png";
+      if (this.item === "Smoking allowed") return symbol = "@/assets/img/smoking.png";
+      if (this.item === "Pets allowed") return symbol = "@/assets/img/paws.png";
+      return symbol;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
